ca761188aa608944dbbe74acafb7b581a5c30505
Denis <ddujota@internationalsafety.com>
2019-01-11 12:34:44 -0500

    installed node-sass and react-router-dom

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import './NavBar.css';
+import './NavBar.scss';
 
 export default class NavBar extends Component {
   render() {
     return (
       <div className="ui container">
         <div id="icons">
-          <button className="ui blue basic button">Blue</button>
-          <button className="ui violet basic button">Violet</button>
-          <button className="ui purple basic button">Purple</button>
-          <button className="ui pink basic button">Pink</button>
+          <span>
+            <button className="ui blue basic button">Blue</button>
+          </span>
+
+          <span>
+            <button className="ui violet basic button">Violet</button>
+          </span>
+
+          <span>
+            <button className="ui purple basic button">Purple</button>
+          </span>
+
+          <span>
+            <button className="ui pink basic button">Pink</button>
+          </span>
         </div>
       </div>
     );
