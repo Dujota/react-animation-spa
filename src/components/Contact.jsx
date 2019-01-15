073d03aa50344cc7288aeadf9d00296acc98de22
Denis <ddujota@internationalsafety.com>
2019-01-15 00:52:42 -0500

    added styling for ocntact and setup layout and styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,25 +20,35 @@ class Contact extends Component {
     return (
       <div id="contact">
         <ContentContainer header={'Contact'} path={'/'}>
-          <h3 className="ui header intro">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, animi
-            deleniti. Dolor laborum impedit tempora minus adipisci quia tenetur
-            officiis dolores at, possimus atque earum doloribus dolore tempore
-            dicta qui.
-          </h3>
+          <div className="contact-content">
+            <section className="contact-details">
+              <h5 className="contact-me-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+                animi deleniti. Dolor laborum impedit tempora minus adipisci
+                quia tenetur officiis dolores at, possimus atque earum doloribus
+                dolore tempore dicta qui.
+              </h5>
+            </section>
 
-          <form className="ui form">
-            <div className="field">
-              <input type="text" name="name" placeholder="Name" />
-            </div>
-            <div className="field">
-              <input type="text" name="email" placeholder="Email Address" />
-            </div>
+            <section className="form-content">
+              <form className="ui form">
+                <div className="field">
+                  <input type="text" name="name" placeholder="Name" rows="5" />
+                </div>
+                <div className="field">
+                  <input type="text" name="email" placeholder="Email Address" />
+                </div>
 
-            <div className="field">
-              <textarea placeholder="Shoot me a Line!" />
-            </div>
-          </form>
+                <div className="field">
+                  <textarea placeholder="Shoot me a Line!" />
+                </div>
+
+                <button className="ui button submit-button" type="submit">
+                  Send
+                </button>
+              </form>
+            </section>
+          </div>
         </ContentContainer>
       </div>
     );
